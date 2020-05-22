ab117729aab44d4be713c297ca27c1343a661b4f
theminkantoso <Phambaodu25072001@gmail.com>
2020-05-22 20:46:42 +0700

    Animation va them loai ke thu

--- Shootableenemy/Moveandshoot.gd
+++ Shootableenemy/Moveandshoot.gd
@@ -1,16 +1,19 @@
 extends KinematicBody2D
 
+
 var bullet = preload("res://Shootableenemy/Enemybullet.tscn")
 
 var delay = 0.3
 var waited = 0
 var bullet_1 = Vector2(500,-300)
 var bullet_2 = Vector2(-500,-300)
-
+var speed = 300
+var enemy_motion = Vector2(-speed,0)
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	
+	set_physics_process(true)
 	set_process(true)
+
 
 func _process(delta):
 	if(waited > delay):
@@ -19,7 +22,10 @@ func _process(delta):
 	elif(waited <= delay):
 		waited += delta
 
-
+func _physics_process(delta):
+	if is_on_wall():
+		enemy_motion.x = - enemy_motion.x 
+	move_and_slide(enemy_motion)
 
 func rapid_fire():
 	shoot()
@@ -33,14 +39,5 @@ func shoot():
 	bullet2.shoot(bullet_2)
 	get_tree().get_root().add_child(bullet1)
 	get_tree().get_root().add_child(bullet2)
-	
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
-
-
-
-
-
 
 
